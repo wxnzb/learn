3d25dd07b10aa7044be81ea7dfe58e3655157952
wxnzb <3127484862@qq.com>
2023-12-05 21:18:13 +0800

    strA.c

--- strA.c
+++ strA.c
@@ -2,4 +2,15 @@ struct strbuf {
     int len;
     int alloc;
     char *buf;
+}
+//初始化 sb 结构体，容量为 alloc
+void strbuf_init(struct strbuf *sb, size_t alloc)
+{
+    sb->buf=(char*)malloc(alloc*sizeof(char));
+    if(sb->buf==NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+    sb->len=0;
+    sb->alloc=alloc;
 };
