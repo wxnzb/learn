c2cbaac2387635d6ba9ea0e6a4cd9be3968555be
wxnzb <3127484862@qq.com>
2023-12-07 23:19:59 +0800

    strA7.c

--- strA.c
+++ strA.c
@@ -76,3 +76,14 @@ void strbuf_reset(struct strbuf *sb)
    sb->buf[i]='\0';
    sb->len=0;
 }
+//确保在 len 之后 strbuf 中至少有 extra 个字节的空闲空间可用
+void strbuf_grow(struct strbuf *sb, size_t extra)
+{
+    if(sb->alloc<sb->len+1+extra)
+    sb->alloc=sb->len+1+extra;
+    sb->buf=(char*)realloc(sb->buf,sb->alloc*sizeof(char));
+    if(sb->buf==NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+}
