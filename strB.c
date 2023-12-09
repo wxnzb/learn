0ff123024e2831a6d974dc5b273ab2f513bf8f55
wxnzb <3127484862@qq.com>
2023-12-09 23:40:20 +0800

    strB2.c

--- strB.c
+++ strB.c
@@ -8,4 +8,15 @@ void strbuf_grow(struct strbuf *sb, size_t extra)
     {
         exit(EXIT_FAILURE);
     }
-}
+//向 sb 追加长度为 len 的数据 data
+void strbuf_add(struct strbuf *sb, const void *data, size_t len)
+{
+    strbuf_grow(sb,len);
+    // strcat(sb->buf,(char*)data);不对！！
+    for(int i=0;i<len;i++)
+    {
+        sb->buf[sb->len+i]=((char *)data)[i];
+    }
+    sb->buf[sb->len+len]='\0';
+    sb->len=sb->len+len;
+}}
